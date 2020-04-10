bb7a17df75c559276fe7f0e68e9812608dafc9dc
shreyasaxor <31658883+shreyasaxor@users.noreply.github.com>
2020-04-11 02:35:43 +0530

    Update settings.py
    allowed hosed and template configuration

--- automate/settings.py
+++ automate/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x*q9-9xiw069^^gz5olr9!qy*d(yc5o%5wu1-8p#cf%u%k$=na'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['shreyasaxor.pythonanywhere.com']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'automate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
